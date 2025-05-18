9760a8debb46950af99e7092901c30d1fa3f8f5d
Alexandria P. <alxpettit@gmail.com>
2025-05-17 19:28:17 -0600

    test

--- src/standard.rs
+++ src/standard.rs
@@ -11,11 +11,13 @@ pub struct StandardMode {
     pin_state: [u8; 2],
     direction: Direction,
     count: i128,
+    prev_clk: bool,
+    prev_dt: bool,
 }
 
 // For debouncing of pins, use 0x0f (b00001111) and 0x0c (b00001100) etc.
-const PIN_MASK: u8 = 0x03;
-const PIN_EDGE: u8 = 0x02;
+// const PIN_MASK: u8 = 0x03;
+// const PIN_EDGE: u8 = 0x02;
 
 impl<DT, CLK> RotaryEncoder<StandardMode, DT, CLK>
 where
@@ -48,34 +50,59 @@ impl StandardMode {
             pin_state: [0xFF, 2],
             direction: Direction::None,
             count: 0,
+            prev_clk: false,
+            prev_dt: false,
         }
     }
 
     /// Update to determine the direction
+    // pub fn update(&mut self, dt_value: bool, clk_value: bool) -> Direction {
+    //     self.pin_state[0] = (self.pin_state[0] << 1) | dt_value as u8;
+    //     self.pin_state[1] = (self.pin_state[1] << 1) | clk_value as u8;
+
+    //     let a = self.pin_state[0] & PIN_MASK;
+    //     let b = self.pin_state[1] & PIN_MASK;
+
+    //     let mut direction: Direction = Direction::None;
+
+    //     let a_is_pin_edge = a == PIN_EDGE;
+    //     let b_is_pin_edge = b == PIN_EDGE;
+
+    //     if a_is_pin_edge {
+    //         self.count += 1;
+    //     }
+    //     if b_is_pin_edge {
+    //         self.count -= 1;
+    //     }
+    //     if a_is_pin_edge && b == 0x00 {
+    //         direction = Direction::Anticlockwise;
+    //     } else if b_is_pin_edge && a == 0x00 {
+    //         direction = Direction::Clockwise;
+    //     }
+
+    //     self.direction = direction;
+
+    //     direction
+    // }
+
     pub fn update(&mut self, dt_value: bool, clk_value: bool) -> Direction {
-        self.pin_state[0] = (self.pin_state[0] << 1) | dt_value as u8;
-        self.pin_state[1] = (self.pin_state[1] << 1) | clk_value as u8;
+        let prev_state = (self.prev_clk as u8) << 1 | self.prev_dt as u8;
+        let curr_state = (clk_value as u8) << 1 | dt_value as u8;
 
-        let a = self.pin_state[0] & PIN_MASK;
-        let b = self.pin_state[1] & PIN_MASK;
+        let direction = match (prev_state, curr_state) {
+            (0b00, 0b01) | (0b01, 0b11) | (0b11, 0b10) | (0b10, 0b00) => {
+                self.count += 1;
+                Direction::Clockwise
+            }
+            (0b00, 0b10) | (0b10, 0b11) | (0b11, 0b01) | (0b01, 0b00) => {
+                self.count -= 1;
+                Direction::Anticlockwise
+            }
+            _ => Direction::None,
+        };
 
-        let mut direction: Direction = Direction::None;
-
-        let a_is_pin_edge = a == PIN_EDGE;
-        let b_is_pin_edge = b == PIN_EDGE;
-
-        if a_is_pin_edge {
-            self.count += 1;
-        }
-        if b_is_pin_edge {
-            self.count -= 1;
-        }
-        if a_is_pin_edge && b == 0x00 {
-            direction = Direction::Anticlockwise;
-        } else if b_is_pin_edge && a == 0x00 {
-            direction = Direction::Clockwise;
-        }
-
+        self.prev_clk = clk_value;
+        self.prev_dt = dt_value;
         self.direction = direction;
 
         direction
